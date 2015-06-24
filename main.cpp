556a2cc3048adb66019b2f4e1bd7e13cfc25a09d
Motoyasue <Ezioleonic@gmail.com>
2015-06-24 08:57:49 +0800

    A address book by C++
    A simple C++ course design.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+
+using namespace std;
+
+struct Contacts
+{
+    string name;
+    string tel;
+    string type;
+    string postcode;
+};
+
+struct CtsAddress
+{
+    linkMan *address;
+    int lenth;
+    int aListSize;
+    int addDepth;
+};
+
+int main(void)
+{
+    Contacts man;
+    CtsAddress manAddress;
+    int i,n;
+    bool flag=true;
+    char yn;
+    char addressList[11];
+}
